062568abdd6c3d6644dad7e93cdca19c287b7fef
JozeMen <jozemennon@gmail.com>
2023-05-13 22:32:04 +0400

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream("temp.bin", FileMode.Open))
+            {
+                long sectionStart = 1024; // Начало секции
+                long sectionLength = 4096; // Длина секции
+
+                using (SectionStreamReader sectionReader = new SectionStreamReader(fileStream, sectionStart, sectionLength))
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    while ((bytesRead = sectionReader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        // Обработка прочитанных данных
+                        // В данном примере мы просто выводим их в консоль
+                        Console.WriteLine($"Read {bytesRead} bytes: {BitConverter.ToString(buffer, 0, bytesRead)}");
+                    }
+                }
+            }
+            Console.ReadKey(); 
+        }
+
+    }
+}
---
+++ ConsoleApp2/SectionStreamReader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+class SectionStreamReader : Stream
+{
+    private Stream baseStream;
+    private long sectionStart;
+    private long sectionLength;
+    private long currentPosition;
+
+    public SectionStreamReader(Stream baseStream, long sectionStart, long sectionLength)
+    {
+        this.baseStream = baseStream;
+        this.sectionStart = sectionStart;
+        this.sectionLength = sectionLength;
+        this.currentPosition = 0;
+    }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => true;
+    public override bool CanWrite => false;
+    public override long Length => sectionLength;
+    public override long Position
+    {
+        get => currentPosition;
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush()
+    {
+        baseStream.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        if (currentPosition >= sectionLength)
+            return 0;
+
+        long bytesToRead = Math.Min(count, sectionLength - currentPosition);
+        int bytesRead = baseStream.Read(buffer, offset, (int)bytesToRead);
+        currentPosition += bytesRead;
+
+        return bytesRead;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        switch (origin)
+        {
+            case SeekOrigin.Begin:
+                currentPosition = Math.Max(0, Math.Min(offset, sectionLength));
+                break;
+            case SeekOrigin.Current:
+                currentPosition = Math.Max(0, Math.Min(currentPosition + offset, sectionLength));
+                break;
+            case SeekOrigin.End:
+                currentPosition = Math.Max(0, Math.Min(sectionLength + offset, sectionLength));
+                break;
+        }
+
+        return currentPosition;
+    }
+
+    public override void SetLength(long value)
+    {
+        baseStream.SetLength(value);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        baseStream.Write(buffer, offset, count);
+    }
+
+    // Implement other overridden methods (Write, Flush, SetLength) if necessary
+}
+
